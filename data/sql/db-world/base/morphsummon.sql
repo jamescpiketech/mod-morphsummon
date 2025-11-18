4630d2116601e88c1d2f08d88d7ee7556fc1130c
James Pike <jamescpike.tech@gmail.com>
2025-11-17 18:37:27 -0600

    Modify NPC and Format Gossip Options

--- data/sql/db-world/base/morphsummon.sql
+++ data/sql/db-world/base/morphsummon.sql
@@ -1,6 +1,6 @@
 SET @ENTRY           := 601072;
-SET @MODELID         := 15665;
-SET @NAME            := 'Cet Keres';
+SET @MODELID         := 23294;
+SET @NAME            := 'Sniggelsby';
 SET @SUBNAME         := 'Polymorphologist';
 SET @SCRIPTNAME      := 'npc_morphsummon';
 SET @NPC_TEXT_HELLO  := @ENTRY;
@@ -21,17 +21,17 @@ INSERT INTO `creature_template_movement` (`CreatureId`, `Ground`, `Swim`, `Fligh
 DELETE FROM `npc_text` WHERE `ID` IN (@NPC_TEXT_HELLO, @NPC_TEXT_SORRY, @NPC_TEXT_CHOICE);
 INSERT INTO `npc_text` (`ID`, `text0_0`) VALUES
 (@NPC_TEXT_HELLO, 'Greetings, $N. If you are looking for ways to change the appearance of your summoned creature, I can help you.'),
-(@NPC_TEXT_SORRY, 'Greetings, $N. I am sorry, but you don''t have a summoned creature that I can polymorph.'),
+(@NPC_TEXT_SORRY, 'Greetings, $N. I am sorry, but you do not have a summoned creature that I can polymorph.'),
 (@NPC_TEXT_CHOICE, 'Please make your choice:');
 
 DELETE FROM `gossip_menu_option` WHERE `MenuID` IN (@MENU_HELLO, @MENU_SORRY, @MENU_CHOICE);
 INSERT INTO `gossip_menu_option` (`MenuID`, `OptionID`, `OptionIcon`, `OptionText`, `OptionBroadcastTextID`, `OptionType`, `OptionNpcFlag`, `ActionMenuID`, `ActionPoiID`, `BoxCoded`, `BoxMoney`, `BoxText`, `BoxBroadcastTextID`, `VerifiedBuild`) VALUES
-(@MENU_HELLO, 0, 0, 'Choose polymorph', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
-(@MENU_HELLO, 1, 0, 'Choose Felguard weapon', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
-(@MENU_SORRY, 0, 0, 'Ah,  nevermind.', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
-(@MENU_CHOICE, 0, 0, 'Back..', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
-(@MENU_CHOICE, 1, 4, 'Next..', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
-(@MENU_CHOICE, 2, 4, 'Previous..', 0, 0, 0, 0, 0, 0, 0, '', 0, 0);
+(@MENU_HELLO, 0, 0, 'Choose Polymorph', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
+(@MENU_HELLO, 1, 0, 'Choose Felguard Weapon', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
+(@MENU_SORRY, 0, 0, 'Ah, nevermind.', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
+(@MENU_CHOICE, 0, 0, 'Back', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
+(@MENU_CHOICE, 1, 4, 'Next', 0, 0, 0, 0, 0, 0, 0, '', 0, 0),
+(@MENU_CHOICE, 2, 4, 'Previous', 0, 0, 0, 0, 0, 0, 0, '', 0, 0);
 
 DELETE FROM `creature_template_model` where `CreatureID` = @ENTRY;
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES (@ENTRY, 0, @MODELID, 1, 1, 12340);
